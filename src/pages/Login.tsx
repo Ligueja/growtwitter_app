fc4613e19f4ececcff280120db168a88be00e55a
Luiz Henrique Coelho <127800714+Ligueja@users.noreply.github.com>
2024-07-11 23:17:32 -0300

    estilo pagina login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,24 @@
-import { Fragment } from "react/jsx-runtime";
+import { CardLoginStyled } from "../components/styled/CardLoginStyled";
+import { ContainerLoginStyled } from "../components/styled/ContainerLoginStyled";
+
+import { LeftLoginStyled } from "../components/styled/LeftLoginStyled";
 
 export function Login() {
   return (
-    <Fragment>
-      <form>
-        <label htmlFor='email'>E-mail</label>
-        <input type='email' name='email' id='email' required />
-
-        <label htmlFor='password'>Senha</label>
-        <input type='password' name='password' id='password' required />
-
-        <button type='submit'>Entrar</button>
-      </form>
-    </Fragment>
+    <ContainerLoginStyled>
+      <CardLoginStyled>
+        <LeftLoginStyled>
+          <h1>Growtwitter</h1>
+          <h3>Trabalho final do bloco intermediário</h3>
+          <p>
+            O Growtwitter é a plataforma definitiva para todos os apaixonados
+            por redes sociais que buscam uma experiência familiar e poderosa,
+            semelhante ao Twitter, mas com um toque único. Seja parte desta
+            comunidade que valoriza a liberdade de expressão, a conexão com
+            pessoas de todo o mundo e a disseminação de ideias.
+          </p>
+        </LeftLoginStyled>
+      </CardLoginStyled>
+    </ContainerLoginStyled>
   );
 }
